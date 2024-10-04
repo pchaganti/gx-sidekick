99c17d8aa1d26d63b49f85319e6c895794e1d9e1
johnbean393 <johnbean393@gmail.com>
2024-10-04 12:28:47 +0800

    Initial Commit

---
+++ Sidekick/SidekickApp.swift
@@ -0,0 +1,17 @@
+//
+//  SidekickApp.swift
+//  Sidekick
+//
+//  Created by Bean John on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SidekickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SidekickTests/SidekickTests.swift
@@ -0,0 +1,17 @@
+//
+//  SidekickTests.swift
+//  SidekickTests
+//
+//  Created by Bean John on 10/4/24.
+//
+
+import Testing
+@testable import Sidekick
+
+struct SidekickTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
