cb6aa365e8b830880f615cebe8fe302bc2403154
johnbean393 <johnbean393@gmail.com>
2025-05-04 13:56:39 +0800

    feat: Update default completions model

--- Sidekick/Logic/Data Models/DownloadManager.swift
+++ Sidekick/Logic/Data Models/DownloadManager.swift
@@ -103,7 +103,7 @@ public class DownloadManager: NSObject, ObservableObject {
 		// Set to not add model
 		self.shouldAddModel = false
 		// Get default model
-		let modelUrl: URL = URL(string: "https://huggingface.co/Triangle104/Dolphin3.0-Qwen2.5-1.5B-Q4_K_M-GGUF/resolve/main/dolphin3.0-qwen2.5-1.5b-q4_k_m.gguf")!
+		let modelUrl: URL = URL(string: "https://huggingface.co/mradermacher/Qwen3-1.7B-Base-GGUF/resolve/main/Qwen3-1.7B-Base.Q4_K_M.gguf")!
 		print("Trying to download \(modelUrl.deletingLastPathComponent().lastPathComponent)")
 		// Download model
 		await self.downloadModel(url: modelUrl)
